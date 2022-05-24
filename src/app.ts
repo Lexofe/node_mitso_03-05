57bc8cca3cb7c5f4afdba9d19806b28ce30789f0
Lexof <77555584+Lexofe@users.noreply.github.com>
2022-05-24 16:40:50 +0300

    lab04

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+
+import express, { NextFunction, Request, Response } from "express";
+
+import dotenv from 'dotenv';
+import { MenuRouter } from "./resources/menu/menu.router";
+import { DishRouter} from './resources/dish/dish.router';
+import { CategoryRouter } from './resources/category/category.router';
+
+dotenv.config();
+const app: express.Application = express();
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  if (req.originalUrl === '/') {
+    res.send('Service is running!');
+    return;
+  }
+  next();
+});
+
+app.use('/menus', MenuRouter);
+app.use('/dishes', DishRouter);
+app.use('/categories', CategoryRouter);
+
+export default app
